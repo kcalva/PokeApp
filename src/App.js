47ca6dcdc4ccc80428fcab20f1c3940182e07775
Kevin <kevin.calva@us.isobar.com>
2023-02-18 15:05:04 -0600

    styled the name of each type. Still need to add rest of data

--- src/App.js
+++ src/App.js
@@ -1,42 +1,30 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import logo from "./assets/images/logo.png"
 import "./App.css"
 import { getPokeTypes } from "./API/API"
 
 const App = () => {
   const [typeData, setTypeData] = useState()
+  useEffect(() => {
+    let fetchData = async () => {
+      let pokeTypesInfo = await getPokeTypes()
+      setTypeData(pokeTypesInfo)
+    }
+    fetchData()
+  })
   return (
     <div className="App">
       <img src={logo} className="App-logo" alt="logo" />
-      <div className="App-container">
-        <header className="App-header">Kevin's Pokemon App</header>
-        <button
-          className="Button"
-          onClick={async () => {
-            let pokeTypesInfo = await getPokeTypes()
-            setTypeData(pokeTypesInfo)
-          }}
-        >
-          Click on this to see api
-        </button>
-        <div>
-          {
-            //TODO: Change this into a method and style
-            typeData
-              ? Object.keys(typeData).map((item, index) => {
-                  return <div key={index}>{item}</div>
-                })
-              : null
-          }
-        </div>
-        <textarea
-          className="Text-area"
-          rows={25}
-          cols={50}
-          value={JSON.stringify(typeData)}
-          readOnly={true}
-        />
-      </div>
+      <div className="App-header">Kevin's Pokemon App</div>
+      {typeData
+        ? Object.keys(typeData).map((typeName, index) => {
+            return (
+              <div id={`Type-${index + 1}`} className="Type-name" key={index}>
+                {typeName.toUpperCase()}
+              </div>
+            )
+          })
+        : null}
     </div>
   )
 }
