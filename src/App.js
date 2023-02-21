6a0c2caaa1d4c68c04d55747bef23b691576d4c1
Kevin <kevin.calva@us.isobar.com>
2023-02-20 21:11:58 -0600

    added some responsive styles and changed name of properties

--- src/App.js
+++ src/App.js
@@ -14,26 +14,30 @@ const App = () => {
   }, [])
   return (
     <div className="App">
-      <img src={logo} className="App-logo" alt="logo" />
+      <img src={logo} alt="logo" />
       <div className="App-header">Kevin's Pokemon App</div>
       {typeData
         ? Object.keys(typeData).map((typeName, index) => {
             return (
               <div key={index}>
-                <div id={`Type-${index + 1}`} className="Type-name">
+                <div
+                  id={typeName.charAt(0).toUpperCase() + typeName.slice(1)}
+                  className="Type-name"
+                >
                   {typeName.toUpperCase()}
                 </div>
                 <div className="App-header">Weak against</div>
                 {typeData[typeName].damage_relations.double_damage_from.map(
-                  (x, index) => (
-                    <div
-                      id={`Type-${x.url.substr(-4, 3).split("/")[1]}`}
-                      key={index}
-                      className={"SubType-name"}
-                    >
-                      {x.name}
-                    </div>
-                  )
+                  (typeDoubleDamage, index) => {
+                    let typeName =
+                      typeDoubleDamage.name.charAt(0).toUpperCase() +
+                      typeDoubleDamage.name.slice(1)
+                    return (
+                      <div id={typeName} key={index} className={"SubType-name"}>
+                        {typeName}
+                      </div>
+                    )
+                  }
                 )}
                 <hr />
               </div>
