860b6bd9861a827286a78abc5962a73062d7e255
Kevin <kevin.calva@us.isobar.com>
2023-03-02 09:20:20 -0600

    refactored some of the code to make it cleaner

--- src/Components/Pokemain.js
+++ src/Components/Pokemain.js
@@ -1,34 +1,12 @@
 import React from "react"
+import Types from "./Types"
 
 const Pokemain = ({ typeData }) => {
   return (
     <div className="Main-container">
       {typeData
         ? Object.keys(typeData).map((typeName, index) => {
-            return (
-              <div key={index} className="Type-container">
-                <div
-                  id={typeName.charAt(0).toUpperCase() + typeName.slice(1)}
-                  className="Type-name"
-                >
-                  {typeName.toUpperCase()}
-                </div>
-                {typeData[typeName].damage_relations.double_damage_from.map(
-                  (typeDoubleDamage, index) => {
-                    let typeName =
-                      typeDoubleDamage.name.charAt(0).toUpperCase() +
-                      typeDoubleDamage.name.slice(1)
-                    return (
-                      /**TODO Start working on styling and adding data to this: */
-                      <div id={typeName} key={index}>
-                        {typeName}
-                      </div>
-                    )
-                  }
-                )}
-                <hr />
-              </div>
-            )
+            return <Types key={index} typeName={typeName} typeData={typeData} />
           })
         : null}
     </div>
